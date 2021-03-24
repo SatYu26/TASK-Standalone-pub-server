9abcbb2cf68a5f34145e4ddfecef18693cf24a28
SatYu26 <goyalsatyam8@gmail.com>
2021-03-24 17:14:10 +0530

    added tests

--- test/gsoc_dart_server_test.dart
+++ test/gsoc_dart_server_test.dart
@@ -1,8 +1,12 @@
-import 'package:gsoc_dart_server/gsoc_dart_server.dart';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('It should return 200 rresponse', () async {
+    final response = await http.get(Uri.http('localhost:8080', '/users'));
+
+    expect(response.statusCode, HttpStatus.ok);
   });
 }
